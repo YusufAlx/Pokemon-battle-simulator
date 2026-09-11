a4ad52db982bd2d983582447135ca32db81b0c5b
YusufAlx <ST10456516@rcconnect.edu.za>
2026-09-11 14:50:17 +0200

    made window,title of window and text on the screen

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+#creates the actual application window
+window = tk.Tk()
+#This names our window
+window.title("Yusufs Pokemon battle Simulator")
+#1000 is the width and 700 is the height of the window 
+window.geometry("1000x700")
+#create a label to display text inside a Window
+title_label = tk.Label(
+    window,
+    text="Pokemon battle Simulator",
+    font=("Arial", 30)
+)
+#this creates a title in the window
+title_label.pack()
+#This starts the event lopp to allow the window to wait for user interaction
+window.mainloop()
+
